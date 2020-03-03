a641cb1b99baa915e9b88ead649ecfaec257b3ba
tayloi <taylor1201am@gmail.com>
2020-03-03 15:09:37 -0500

    Renamed folder and created navbar template

---
+++ Code/navbar.php
@@ -0,0 +1,10 @@
+<?php
+	echo 
+    "<div id='nav'>
+        <h1 id='siteTitle'>Group 9 Project</h1>
+        <a class='navItem' href='index.html'>Home</a>
+        <a class='navItem' href='account.html'>Account</a>
+        <a class='navItem' href='learn.html'>Learn</a>
+        <a class='navItem' href='logout.php'>Log out</a>
+    </div>";
+?>
