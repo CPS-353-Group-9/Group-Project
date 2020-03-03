8c5620c55b5c06d2c6072650c4e6699e6f1aabd6
cacciopl1 <61178052+cacciopl1@users.noreply.github.com>
2020-03-03 12:13:10 -0500

    Create signup.php

---
+++ No Framework/signup.php
@@ -0,0 +1,11 @@
+<main>
+	<h1>Signup</h1>
+	<a href="index.html">Login</a>
+	<form action = "includes/signup_i.php" method="post">
+		<input type="text" name="user" placeholder="Username">
+		<input type="text" name="email" placeholder="Email">
+		<input type="password" name="pass" placeholder="Password">
+		<input type="password" name="pass_repeat" placeholder="Repeat Password">
+		<button type="submit" name="signup-submit">Signup</button>
+	</form>
+</main>
