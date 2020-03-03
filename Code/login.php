e1da8d03e6465084ad29778fb193d1f1092ddc1c
tayloi <taylor1201am@gmail.com>
2020-03-03 16:27:36 -0500

    Updated navbar and added customStyles

---
+++ Code/login.php
@@ -0,0 +1,5 @@
+<form action = "includes/login_i.php" method = "post">
+    <input type="text" name="email" placeholder="Username">
+    <input type="password" name="pass" placeholder="Password">
+    <button type = "submit" name="login-submit">Login</button>
+</form>
--- Code/navbar.php
+++ Code/navbar.php
@@ -1,10 +1,58 @@
 <?php
 	echo 
-    "<div id='nav'>
-        <h1 id='siteTitle'>Group 9 Project</h1>
-        <a class='navItem' href='index.html'>Home</a>
-        <a class='navItem' href='account.html'>Account</a>
-        <a class='navItem' href='learn.html'>Learn</a>
-        <a class='navItem' href='logout.php'>Log out</a>
-    </div>";
+    '<nav class="navbar" role="navigation" aria-label="main navigation" style="background-color: #f58426;">
+        <div class="navbar-brand">
+        <a class="navbar-item" href="index.html">
+            <img alt="Group 9 Logo " width="112" height="28">
+        </a>
+    
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+        </div>
+    
+        <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item primary">
+            Home
+            </a>
+    
+            <a class="navbar-item primary">
+            Learn
+            </a>
+    
+            <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link primary">
+                Account
+            </a>
+            <div class="navbar-dropdown">
+                <a class="navbar-item secondary">
+                Stats
+                </a>
+                <a class="navbar-item secondary">
+                Settings
+                </a>
+            </div>
+            </div>
+        </div>
+    
+        <div class="navbar-end">
+            <div class="navbar-item">
+            <div class="buttons">
+                <a class="button primary" href="signup.php">
+                <strong>Sign up</strong>
+                </a>
+                <a class="button secondary">
+                Log in
+                </a>
+                <a class="button secondary" type = "submit" name="logout-submit">
+                    Log out
+                </a>
+            </div>
+            </div>
+        </div>
+        </div>
+    </nav>';
 ?>
