66ef582958da16261c89886bfa5b1d5a5375fcda
tayloi <taylor1201am@gmail.com>
2020-03-08 19:40:53 -0400

    Updated login.php and index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<?php
-	require "header.php";
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Group 9 Project</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		<link rel="stylesheet" href="customStyles.css">
+	</head>	
 
-	<main>
-
-	</main>
+	<body>
+		<?php include 'navbar.php';?>
+		<header> Group 9 Project </header>
+		<h3>Welcome to the Home Page.</h3>
+	</body>
+</html>
--- login.php
+++ login.php
@@ -1,5 +1,25 @@
-<form action = "includes/login_i.php" method = "post">
-    <input type="text" name="email" placeholder="Username">
-    <input type="password" name="pass" placeholder="Password">
-    <button type = "submit" name="login-submit">Login</button>
-</form>
+<?php
+    //login functionality goes here
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Group 9 Project</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		<link rel="stylesheet" href="customStyles.css">
+	</head>	
+
+	<body>
+		<?php include 'navbar.php';?>
+		<header> Group 9 Project </header>
+        <h3>Login Page</h3>
+        
+        <form action = "includes/login_i.php" method = "post">
+            <input type="text" name="email" placeholder="Username">
+            <input type="password" name="pass" placeholder="Password">
+            <button type = "submit" name="login-submit">Login</button>
+        </form>
+	</body>
+</html>
