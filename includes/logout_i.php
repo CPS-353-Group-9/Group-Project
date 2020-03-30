716cdfae858ea332a13d88cbd0856b4918e9efde
lscaccio <luccaccioppoli@yahoo.com>
2020-03-30 10:15:04 -0400

    Update logout_i.php

--- includes/logout_i.php
+++ includes/logout_i.php
@@ -3,5 +3,5 @@
 	session_unset(); // deletes all the values from current session
 	session_destroy(); // destroys current session on the website (logs out user so a new user with different values can use the website)
 
-	header("Location: ../index.php"); // takes user back to the home page
+	header("Location: ../index.php?logout=success"); // takes user back to the home page
 ?>
