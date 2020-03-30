af8a0d341978c92eea54eb4aed7352f67b2473fc
lscaccio <luccaccioppoli@yahoo.com>
2020-03-30 10:15:10 -0400

    Update login.php

--- login.php
+++ login.php
@@ -27,6 +27,23 @@
         <form action = "includes/login_i.php" method = "post">
             <div id = "fid1"><input type="text" name="email" placeholder="Username">
             <input type="password" name="pass" placeholder="Password"></div>
+			<?php
+				if (isset($_GET["error"])) // getting the error checks from 'login_i.php' and showing the user what they inputed incorrectly
+				{
+					if ($_GET["error"] == "emptyfields")
+					{
+						echo '<p> You must fill all fields. </p>';
+					}
+					else if ($_GET["error"] == "wrongpassword")
+					{
+						echo '<p> Incorrect username or password. </p>';
+					}
+					else if ($_GET["error"] == "nouser")
+					{
+						echo '<p> Incorrect username or password. </p>';
+					}
+				}
+			?>
             <div id = "fid1"><button type = "submit" name="login-submit">Log In</button></div>
         </form>
 		</div>
