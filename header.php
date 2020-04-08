946d20beb706d0f46c8c113937ff183ef2f02afe
skylerdyoung <skylerdyoung95@gmail.com>
2020-04-08 14:09:43 -0400

    header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<head>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		<link rel="stylesheet" type="text/css" href="customStyles.css?id=12345">
+		<link rel="stylesheet" type="text/css" href="newStyles1.css?id=1234">
+		<title>Easy As Pie-thon</title>
+</head>	
--- login.php
+++ login.php
@@ -8,13 +8,9 @@
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8" />
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-		<link rel="stylesheet" href="customStyles.css">
-		<link rel="stylesheet" href="newStyles1.css">
-		<title>Easy As Pie-thon</title>
-	</head>		
+	<?php
+		include "header.php";
+	?>
 
 	<body>
 		<?php include 'navbar.php';?>
