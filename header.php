24c1467fe3cf190972c16973952056fba85e9a78
skylerdyoung <skylerdyoung95@gmail.com>
2020-04-10 17:58:20 -0400

    css merge

--- header.php
+++ header.php
@@ -2,6 +2,5 @@
 		<meta charset="UTF-8" />
 		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
 		<link rel="stylesheet" type="text/css" href="customStyles.css?id=12345">
-		<link rel="stylesheet" type="text/css" href="newStyles1.css?id=1234">
 		<title>Easy As Pie-thon</title>
 </head>	
