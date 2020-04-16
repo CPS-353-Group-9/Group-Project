fb4e51e2316ca8e22187bdaccab9110f07aae47d
tayloi <taylor1201am@gmail.com>
2020-04-16 18:51:59 -0400

    Created more chapters

---
+++ ch3.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+
+Test page for the Learn option.
+
+*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<?php
+		include "header.php";
+	?>
+
+	<body>
+		<?php include 'navbar.php';?>
+		<h3>Chapter 3</h3>
+		
+		<div class="textbook-body">
+			<section>
+				<h4>Subtitle 1</h4>
+					
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum at ex in lobortis. Mauris mattis, odio eu hendrerit accumsan, nisl eros mollis urna, vitae lacinia lorem orci quis dui. Morbi vitae ullamcorper lectus, et faucibus nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam a lectus et eleifend. Praesent dui elit, interdum vel auctor id, maximus nec purus. Aliquam sit amet quam condimentum, auctor leo ac, condimentum ipsum.</p>
+				
+				<p>Sed at porttitor diam. Fusce vel tempus dolor, dapibus sollicitudin quam. Donec convallis sagittis rhoncus. Integer sit amet consectetur felis, id condimentum nisi. Duis viverra, metus eu efficitur dapibus, magna libero ultrices neque, vel vestibulum arcu massa eget purus. Mauris nisi arcu, sodales ut placerat at, posuere convallis diam. Donec congue, nisl at consectetur tempus, libero lacus malesuada lacus, in tempor odio elit at turpis. Fusce feugiat neque sit amet lacinia vulputate. Praesent bibendum lectus eu laoreet porttitor. Curabitur non lorem iaculis, eleifend ante ac, tristique metus. Sed eget nisi eleifend, varius nibh eu, cursus nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer et massa nisl. Sed lacinia et turpis sed hendrerit.</p>
+			</section>
+			
+			<section>
+				<h4>Subtitle 2</h4>
+
+				<p>Suspendisse potenti. Aliquam erat volutpat. Mauris finibus risus sapien, ut porta turpis viverra id. Donec vel mollis leo. Maecenas ut sapien et sem consectetur iaculis maximus sit amet lorem. Mauris ac metus cursus, posuere leo quis, ullamcorper velit. Quisque sed imperdiet augue, in iaculis est.</p>
+			</section>
+
+			<section>
+				<h4>Subtitle 3</h4>
+
+				<p>Pellentesque dictum eros turpis, pharetra consectetur lectus vestibulum vitae. Maecenas pellentesque molestie ex eget placerat. Fusce elementum lobortis mi, nec vestibulum erat volutpat at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla justo ligula, placerat non nibh feugiat, faucibus varius quam. Sed auctor efficitur risus, vitae semper turpis consectetur at. Quisque lobortis, tortor eu eleifend semper, massa dolor accumsan ex, sit amet fringilla augue tellus in magna. Praesent dui urna, consequat eu lectus sed, convallis interdum orci. Nam a vehicula leo. Donec commodo neque in sapien elementum, ac volutpat orci venenatis. Vestibulum venenatis erat sapien, non iaculis metus aliquam non. Cras venenatis accumsan ipsum id blandit. Proin sit amet vestibulum lacus. Vestibulum pulvinar risus ac tincidunt mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+			</section>
+		</div>
+        
+        <br> <br>
+
+		<footer>
+			<p>	<a class="link" href="explore.php">Go back to chapter list</a> <br> </p>
+		</footer>
+	</body>
+</html>
