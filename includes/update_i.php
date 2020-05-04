a60717b90a35c17b0fc2c4033bd3735bdcbef57a
lscaccio <luccaccioppoli@yahoo.com>
2020-05-04 18:11:24 -0400

    Update update_i.php

--- includes/update_i.php
+++ includes/update_i.php
@@ -55,44 +55,10 @@
 					$_SESSION['occ'] = $row['occupation'];
                     $_SESSION['user_level'] = $row['user_level'];
                 }
-                
-
-                
-                /*
-                $sql = "SELECT * FROM User WHERE Username='$_POST[prevuser]'";
-                $row = mysqli_fetch_array($sql);
-
-                $_SESSION['email'] = $row['Email'];
-                $_SESSION['userId'] = $row['ID'];
-                $_SESSION['user'] = $row['Username'];
-                $_SESSION['level'] = $row['Level'];
-                $_SESSION['create'] = $row['createDate'];
-                $_SESSION['first'] = $row['FName'];
-                $_SESSION['last'] = $row['LName'];
-                $_SESSION['mi'] = $row['MI'];
-                $_SESSION['country'] = $row['Country'];
-                $_SESSION['state'] = $row['State'];
-                $_SESSION['city'] = $row['City'];
-                $_SESSION['occ'] = $row['Occupation'];
-                */
                 header("Location: ../settings.php?update=success");
                 exit();
             }
         }
-        else if(!empty($account) && !empty($username) && !empty($email) && !empty($fname) && !empty($lname) && !empty($mi) && !empty($country) && !empty($state) && !empty($city) && !empty($occupation)) // all fields filled
-        {
-            //update query
-            $sql = "UPDATE user_profile SET user_name = '$_POST[prevuser]', email = '$_POST[email]', first_name = '$_POST[first]', last_name = '$_POST[last]', middle_initial = '$_POST[middle]', country = '$_POST[country]', state = '$_POST[state]', city = '$_POST[city]', occupation = '$_POST[occupation]' WHERE user_name = '$_POST[prevuser]'";
-
-            //execute
-            if (mysqli_query($connect, $sql))
-            {
-                header("Location: ../settings.php?update=success");
-                exit();
-            }
-
-        }
-
     }
     else if(isset($_POST['back-submit']))
     {
