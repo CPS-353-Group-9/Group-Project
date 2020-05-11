2298ab28a9dde798777c6955b1aa2d974d67b679
skylerdyoung <skylerdyoung95@gmail.com>
2020-05-11 13:21:50 -0400

    Update test1.php
    pulls questions from database instead of having them coded onto the page

--- test1.php
+++ test1.php
@@ -19,66 +19,25 @@ Test page for the Learn option.
 		<h3>Test Your Knowledge</h3>
 
 		<?php
+			require "includes/db_i.php";
+		
 			if (isset($_SESSION['userId'])) // if logged in display the chapters
 			{
 				echo('<div class="textbook-body">');
 				
 				echo('<form action="test1res.php" method="post" id="quiz">');
 
-				echo("<h6>" . "What does a variable do?" . "</h6>");
+				$sql = "SELECT question_1, question_2, question_3, question_4, question_5 FROM questions WHERE TEID = 1";
+				$result = mysqli_query($connect, $sql);
 				
-				echo('<input type="radio" id="1" name="1" id="1-A" value = "A"  >
-				<label for="1-A">Changes your password.</label><br>');
-				echo('<input type="radio" id="1" name="1" id="1-B" value = "B" >
-				<label for="1-B">Store different types of data.</label><br>');
-				echo('<input type="radio" id="1" name="1" id="1-C" value = "C" >
-				<label for="1-C">Acts as a placeholder.</label><br>');
-				echo('<input type="radio" id="1" name="1" id="1-D" value = "D" >
-				<label for="1-D">None of the above.</label><br>');
+				$row = mysqli_fetch_assoc($result);
 				
-				echo("<br/><h6>" . "What does a print statement do?" . "</h6>");
-				
-					echo('<input type="radio" id="2" name="2" value = "A" >
-				<label for="2-A">Outputs something to the console.</label><br>');
-				echo('<input type="radio" id="2" name="2" value = "B" >
-				<label for="2-B">Activates your printer.</label><br>');
-				echo('<input type="radio" id="2" name="2" value = "C" >
-				<label for="2-C">Changes the background color.</label><br>');
-				echo('<input type="radio" id="2" name="2" value = "D" >
-				<label for="2-D">None of the above.</label><br>');
-				
-				echo("<br/><h6>" . "Can Python be used as a calculator?" . "</h6>");
-				
-					echo('<input type="radio" id="3" name="3" value = "A" >
-				<label for="3-A">Yes.</label><br>');
-				echo('<input type="radio" id="3" name="3" value = "B" >
-				<label for="3-B">No.</label><br>');
-				echo('<input type="radio" id="3" name="3" value = "C" >
-				<label for="3-C">Only after backing up your hard drive.</label><br>');
-				echo('<input type="radio" id="3" name="3" value = "D" >
-				<label for="3-D">Only after executing a print statement.</label><br>');
-				
-				echo("<br/><h6>" . "What is programming?" . "</h6>");
-				
-					echo('<input type="radio" id="4" name="4" value = "A" >
-				<label for="4-A">Using computers in any way.</label><br>');
-				echo('<input type="radio" id="4" name="4" value = "B" >
-				<label for="4-B">Reconnecting lost internet connections.</label><br>');
-				echo('<input type="radio" id="4" name="4" value = "C" >
-				<label for="4-C">Telling the computer to do something through a specialized language.</label><br>');
-				echo('<input type="radio" id="4" name="4" value = "D" >
-				<label for="4-D">None of the above.</label><br>');
-				
-				echo("<br/><h6>" . "What is the name of this website?" . "</h6>");
-				
-					echo('<input type="radio" id="5" name="5" value = "A" >
-				<label for="5-A">Pie Simple</label><br>');
-				echo('<input type="radio" id="5" name="5" value = "B" >
-				<label for="5-B">Easy As Pie-thon</label><br>');
-				echo('<input type="radio" id="5" name="5" value = "C" >
-				<label for="5-C">Easy Slice of Pie</label><br>');
-				echo('<input type="radio" id="5" name="5" value = "D" >
-				<label for="5-D">Easy A Programming</label><br>');
+				foreach($row as &$value){
+					if ($value != null){
+						echo $value;
+					}
+				}
+
 				echo('<br/>');
 				
 				echo('</div>');
