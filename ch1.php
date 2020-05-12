450059977c4029b24a0028e61391ea8bddc3cbdf
skylerdyoung <skylerdyoung95@gmail.com>
2020-05-12 13:47:21 -0400

    ch
    database code

--- ch1.php
+++ ch1.php
@@ -17,6 +17,27 @@ Test page for the Learn option.
 	<body>
 		<?php include 'navbar.php';?>
 		
+		<?php
+				require "includes/db_i.php";
+		
+				$sql = "SELECT ch_text FROM chapters WHERE CHID = 1";
+				$result = mysqli_query($connect, $sql);
+			
+				$row = mysqli_fetch_assoc($result); //creates an array of values from the result
+				
+				$chapter_text = "";
+				
+				foreach($row as &$value){ //pulls values out of array
+				
+					$chapter_text = $value;
+					
+				}
+				
+				echo $chapter_text;
+				
+		
+		?>
+		
 		<footer>
 			<p>	<a class="link" href="explore.php">Go back to chapter list</a> <br> </p>
 		</footer>
